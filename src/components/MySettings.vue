661f605f364663aef87fbd3f0a7bd657a5468cf4
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-07-16 20:51:01 +0700

    tidy up config

--- src/components/MySettings.vue
+++ src/components/MySettings.vue
@@ -6,22 +6,49 @@ const cfg = useConfig();
 
 <template>
 	<div class="q-px-lg plugin-base">
-		Configure plugin
-		<div>
-			<label>
-				Server endpoint URL
-				<input type="text" v-model="cfg.endpoint" />
-			</label>
-			<label>
-				<input type="radio" />
-				Polling
-			</label>
-			<label>
-				<input type="radio" />
-				WebSockets
-			</label>
+		<div class="container">
+			<span>Configure plugin</span>
+			<div>
+				<label>
+					Server endpoint URL
+					<input type="text" v-model="cfg.endpoint" />
+				</label>
+			</div>
+			<div>
+				<label>
+					<input
+						type="radio"
+						disabled
+						v-model="cfg.websockets"
+						v-bind:value="true"
+					/>
+					WebSockets
+				</label>
+				<label>
+					<input
+						type="radio"
+						disabled
+						v-model="cfg.websockets"
+						v-bind:value="false"
+					/>
+					Polling
+				</label>
+			</div>
+			<div>
+				<button type="button" class="c-btn primary">Refresh</button>
+			</div>
+		</div>
+		<div class="container">
+			<span>Status</span>
 		</div>
 	</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+	margin-bottom: 2rem;
+	display: flex;
+	flex-direction: column;
+	gap: 0.5rem;
+}
+</style>
